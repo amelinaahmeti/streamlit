e06929516965bb72a903143566d5ba7c52f1940f
amelinaahmeti <amelina.ahmeti@fscj.edu>
2026-02-27 15:28:37 -0500

    Update uber_nyc_pickup.py

--- uber_nyc_pickup.py
+++ uber_nyc_pickup.py
@@ -10,11 +10,11 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 
 @st.cache_data
 def load_data(nrows): 
-data = pd.read_csv(DATA_URL, nrows=nrows) 
-lowercase = lambda x: str(x).lower() 
-data.rename(lowercase, axis='columns', inplace=True) 
-data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN]) 
-return data 
+    data = pd.read_csv(DATA_URL, nrows=nrows) 
+    lowercase = lambda x: str(x).lower() 
+    data.rename(lowercase, axis='columns', inplace=True) 
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN]) 
+    return data 
 
 # Create a text element and let the reader know the data is loading. 
 data_load_state = st.text('Loading data...') 
